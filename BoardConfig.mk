7055c892b857bc9fadfa3aa8fea477542e999ae3
extreme <shinema3743@gmail.com>
2025-06-04 17:27:41 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,10 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-# Inherit from lenovo mt6765-common
--include device/lenovo/mt6765-common/BoardConfigCommon.mk
-
 DEVICE_PATH := device/lenovo/m8
 
 # Board
@@ -27,3 +23,83 @@ TARGET_KERNEL_CONFIG := akita_row_wifi_defconfig
 
 # OTA
 TARGET_OTA_ASSERT_DEVICE := m8
+
+BOARD_VENDOR := xiaomi
+
+BUILD_BROKEN_DUP_RULES := true
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+TARGET_USES_64_BIT_BINDER := true
+
+# Audio
+USE_XML_AUDIO_POLICY_CONF := 1
+
+# Board
+TARGET_NO_BOOTLOADER := true
+
+# HIDL
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
+
+# Kernel
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32S1,32S1
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x11b00000 --second_offset 0x00f00000 --tags_offset 0x07880000
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_SOURCE := kernel/xiaomi/mt6765
+
+# Lineage hardware
+BOARD_HARDWARE_CLASS += \
+    $(COMMON_PATH)/lineagehw
+
+# Network Routing
+TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+#BOARD_VENDORIMAGE_PARTITION_SIZE := 805306368
+#BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+
+# Power
+TARGET_TAP_TO_WAKE_NODE := /proc/touchpanel/double_tap_enable
+
+# Properties
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/recovery.fstab
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
+
+# SELinux
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
+
+# Symbols
+TARGET_LD_SHIM_LIBS := /system/lib/libshowlogo.so|libshim_showlogo.so
+
+# Treble
+BOARD_VNDK_VERSION  := current
+PRODUCT_EXTRA_VNDK_VERSIONS := 28
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
